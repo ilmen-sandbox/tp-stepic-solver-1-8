9177991d400c76b6cd918e7951084a0ab3fe183b
Viacheslav Shaposhnikov <shaposhnikov89@gmail.com>
2016-03-12 10:31:30 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default.cong
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo service nginx stop
 sudo service nginx start
